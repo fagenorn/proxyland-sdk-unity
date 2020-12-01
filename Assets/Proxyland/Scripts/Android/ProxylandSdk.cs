1ea9743daa3abcd364e9add0b907e80ed1dea154
fagenorn <anisanissakkaf@gmail.com>
2020-12-01 11:12:07 +0100

    Fixed crash caused in editor

--- Assets/Proxyland/Scripts/Android/ProxylandSdk.cs
+++ Assets/Proxyland/Scripts/Android/ProxylandSdk.cs
@@ -2,6 +2,7 @@
 
 public class Proxyland
 {
+#if UNITY_ANDROID && !UNITY_EDITOR
     private static readonly AndroidJavaClass _proxylandClass = new AndroidJavaClass("com.betroix.proxyland.ProxylandSdk");
 
     private static readonly AndroidJavaClass _unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
@@ -9,6 +10,7 @@ public class Proxyland
     private static readonly AndroidJavaObject _activity = _unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
 
     private static readonly AndroidJavaObject _context = _activity.Call<AndroidJavaObject>("getApplicationContext");
+#endif
 
     public static void Init(string parnterId, string apiKey)
     {
