19f7f4247193f6a33bf7fea583c8414bf29e44d9
fagenorn <anisanissakkaf@gmail.com>
2020-12-04 13:33:26 +0100

    Updated unity sdk script + 1.2v SDK

--- Assets/Proxyland/Scripts/Android/ProxylandSdk.cs
+++ Assets/Proxyland/Scripts/Android/ProxylandSdk.cs
@@ -1,22 +1,32 @@
 ﻿using UnityEngine;
 
-public class Proxyland
+public static class Proxyland
 {
 #if UNITY_ANDROID && !UNITY_EDITOR
-    private static readonly AndroidJavaClass _proxylandClass = new AndroidJavaClass("com.betroix.proxyland.ProxylandSdk");
-
-    private static readonly AndroidJavaClass _unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-
-    private static readonly AndroidJavaObject _activity = _unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-
-    private static readonly AndroidJavaObject _context = _activity.Call<AndroidJavaObject>("getApplicationContext");
+    private static AndroidJavaObject _proxylandStatic = null;
+    private static AndroidJavaObject _currentActivity = null;
 #endif
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Init()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if ( Application.platform == RuntimePlatform.Android )
+        {
+            using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            using (var proxylandClass = new AndroidJavaClass("com.betroix.proxyland.ProxylandSdk"))
+            {
+                _currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+                _proxylandStatic = proxylandClass.GetStatic<AndroidJavaObject>("Companion");
+            }
+        }
+#endif
+    }
 
     public static void Init(string parnterId, string apiKey)
     {
 #if UNITY_ANDROID && !UNITY_EDITOR
-        var companion = _proxylandClass.GetStatic<AndroidJavaObject>("Companion");
-        companion.Call("initializeAsync", _context, parnterId, apiKey, null, null);
+        _proxylandStatic.Call("initializeAsync", _currentActivity, parnterId, apiKey, null, null);
 #endif
     }
 }
